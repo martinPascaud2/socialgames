910e2ae132376569a4c7d3d870c907b3437f59b9
Code <Code@MacBook-Pro-de-Martin.local>
2024-02-08 15:57:19 +0100

    viewport (pending)

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,16 @@ export const metadata = {
   description: "Application de jeux sociaux",
 };
 
+export const viewport = {
+  width: "device-width",
+  height: "device-height",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
+};
+// "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover",
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
