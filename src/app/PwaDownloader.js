3acb2163e2d3c5f07f2457e54b62a175163baddc
Code <Code@MacBook-Pro-de-Martin.local>
2024-02-16 02:58:22 +0100

    iphone add_app (pending)

--- src/app/PwaDownloader.js
+++ src/app/PwaDownloader.js
@@ -2,10 +2,15 @@
 
 import { useEffect, useState } from "react";
 
+import Image from "next/image";
+import ShareIcon from "./ShareIcon.png";
+import Plus from "./Plus.png";
+
 const isBrowser = () => typeof window !== "undefined";
 
 export default function PwaDownloader() {
   const [deferredPrompt, setDeferredPrompt] = useState();
+  const [showInstallMessage, setShowInstallMessage] = useState(false);
 
   const installApp = async () => {
     if (deferredPrompt) {
@@ -23,7 +28,37 @@ export default function PwaDownloader() {
         setDeferredPrompt(e);
       });
     }
+
+    const isIos = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod/.test(userAgent);
+    };
+    const isInStandaloneMode = () =>
+      "standalone" in window.navigator && window.navigator.standalone;
+    if (isIos() && !isInStandaloneMode()) {
+      setShowInstallMessage(true);
+    }
   }, []);
+
+  console.log("showInstallMessage", showInstallMessage);
+
+  if (showInstallMessage)
+    return (
+      <div className="absolute bottom-10 w-screen border border-blue-300 bg-blue-100 pt-2 flex flex-col items-center">
+        <div>Installe Social Games sur ton iPhone !</div>
+        <div>
+          Clique sur
+          <span className="inline-flex align-middle mb-2">
+            <Image src={ShareIcon} alt="share-icon" className="h-7 w-8" />
+          </span>
+          {/* , puis &quot;Ajouter à l&apos;écran d&apos;accueil&quot; */}, puis
+          sur{" "}
+          <span className="inline-flex align-middle mb-2">
+            <Image src={Plus} alt="add-icon" className="h-6 w-6" />
+          </span>
+        </div>
+      </div>
+    );
 
   if (!deferredPrompt) return null;
 
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import PwaDownloader from "./PwaDownloader";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <LoginForm />
       <GuestLink setCookieToken={setCookieToken} />
       <PwaDownloader />
