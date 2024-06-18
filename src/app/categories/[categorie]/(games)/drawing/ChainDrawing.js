f60a60554312834780a0955bc0c303064186fa24
Code <Code@MacBook-Pro-de-Martin.local>
2024-06-18 03:04:07 +0200

    NextStep_ChainDrawing

--- src/app/categories/[categorie]/(games)/drawing/ChainDrawing.js
+++ src/app/categories/[categorie]/(games)/drawing/ChainDrawing.js
@@ -143,187 +143,190 @@ export default function ChainDrawing({ roomId, roomToken, user, gameData }) {
   }, [phase]);
 
   return (
-    <>
-      <div>Esquissé</div>
-
-      {phase === "waiting" && (
+    <div className="overflow-y-auto">
+      {!gameData.ended && (
         <>
-          {!isEven ? (
+          {phase === "waiting" && (
             <>
-              <div>
-                Votre mot de départ :{" "}
-                <span className="font-semibold">{chainRef?.word}</span>
-              </div>
-              {!hasValidated ? (
-                <button
-                  onClick={() => {
-                    goNextPhase({ roomToken, gameData });
-                    setHasValidated(true);
-                  }}
-                  className="border border-blue-300 bg-blue-100"
-                >
-                  Ok !
-                </button>
-              ) : (
-                <div>On attend les autres joueurs</div>
-              )}
-            </>
-          ) : (
-            <>
-              {isAdmin ? (
-                <div className="fixed bottom-0 z-10 left-1/2 translate-x-[-50%] translate-y-[-25%]">
-                  <NextStep
-                    onClick={() =>
-                      goNextPhase({ roomToken, gameData, full: true })
-                    }
-                  >
-                    Lancer
-                  </NextStep>
-                </div>
-              ) : (
-                <div>Ça va commencer !</div>
-              )}
-            </>
-          )}
-        </>
-      )}
-
-      {phase === "drawing" && lastLink?.type === "word" && (
-        <>
-          {!hasValidated ? (
-            <>
-              <div className="flex justify-center">
-                Mot à dessiner :
-                <span className="font-semibold">
-                  &nbsp;{lastLink && lastLink.data}
-                </span>
-              </div>
-              <Draw
-                setImgData={setData}
-                //   setSvg={setSvg}
-                //   setPath={setPath}
-                setHasValidated={setHasValidated}
-                finishCountdownDate={finishCountdownDate}
-              />
-            </>
-          ) : (
-            <div className="flex justify-center">
-              Hop ! C&apos;est envoyé ! On attend les autres...
-            </div>
-          )}
-
-          <div className="flex justify-center">
-            <CountDown finishCountdownDate={finishCountdownDate} />
-          </div>
-        </>
-      )}
-
-      {phase === "guessing" &&
-        lastLink?.type === "draw" &&
-        lastLink?.data.startsWith("data:image") && (
-          <>
-            <div
-              style={{
-                position: "relative",
-                width: "auto",
-                height: "50vh",
-                // left: "5vw",
-              }}
-            >
-              <Image
-                src={lastLink.data}
-                alt="drawing-png"
-                // sizes="500px"
-                fill
-                style={{
-                  objectFit: "contain",
-                }}
-              />
-            </div>
-            {!hasValidated ? (
-              <>
-                <div>Qu&apos;est-ce que ça représente ???</div>
-                <input
-                  type="text"
-                  value={guess}
-                  onChange={(e) => setGuess(e.target.value)}
-                  className="border focus:outline-none focus:border-2"
-                />
-
-                <button
-                  onClick={() => {
-                    setData(guess);
-                    setHasValidated(true);
-                  }}
-                  className="border border-blue-300 bg-blue-100"
-                >
-                  Envoi
-                </button>
-              </>
-            ) : (
-              <div>Envoyé ! On attend les autres...</div>
-            )}
-          </>
-        )}
-
-      {phase.startsWith("showing") && (
-        <>
-          <div>Chaîne de {showedGamer}</div>
-
-          {showedLinks && (
-            <div>
-              {showedLinks.map((link) => (
+              {!isEven ? (
                 <>
-                  {link.type === "draw" ? (
-                    <>
-                      <div>Dessin de {link.userName}</div>
-                      <div
-                        style={{
-                          position: "relative",
-                          width: "auto",
-                          height: "50vh",
-                          // left: "5vw",
-                        }}
-                      >
-                        <Image
-                          src={link.data}
-                          alt="drawing-png"
-                          // sizes="500px"
-                          fill
-                          style={{
-                            objectFit: "contain",
-                          }}
-                        />
-                      </div>
-                    </>
-                  ) : showedGamer !== link.userName ? (
-                    <div>
-                      {link.userName} pense qu&apos;il s&apos;agit de :{" "}
-                      {link.data}
-                    </div>
+                  <div>
+                    Votre mot de départ :{" "}
+                    <span className="font-semibold">{chainRef?.word}</span>
+                  </div>
+                  {!hasValidated ? (
+                    <button
+                      onClick={() => {
+                        goNextPhase({ roomToken, gameData });
+                        setHasValidated(true);
+                      }}
+                      className="border border-blue-300 bg-blue-100"
+                    >
+                      Ok !
+                    </button>
                   ) : (
-                    <div>
-                      Et il s&apos;agissait de :{" "}
-                      <span className="font-semibold">{link.data}</span>
-                    </div>
+                    <div>On attend les autres joueurs</div>
                   )}
                 </>
-              ))}
-            </div>
+              ) : (
+                <>
+                  {isAdmin ? (
+                    <div className="fixed bottom-0 z-10 left-1/2 translate-x-[-50%] translate-y-[-25%]">
+                      <NextStep
+                        onClick={() =>
+                          goNextPhase({ roomToken, gameData, full: true })
+                        }
+                      >
+                        Lancer
+                      </NextStep>
+                    </div>
+                  ) : (
+                    <div>Ça va commencer !</div>
+                  )}
+                </>
+              )}
+            </>
           )}
 
-          {isShower && (
-            <button
-              onClick={() => {
-                goNextShow({ roomToken, gameData });
-              }}
-              className="border border-blue-300 bg-blue-100"
-            >
-              Afficher la suite
-            </button>
+          {phase === "drawing" && lastLink?.type === "word" && (
+            <>
+              {!hasValidated ? (
+                <>
+                  <div className="flex justify-center">
+                    Mot à dessiner :
+                    <span className="font-semibold">
+                      &nbsp;{lastLink && lastLink.data}
+                    </span>
+                  </div>
+                  <Draw
+                    setImgData={setData}
+                    //   setSvg={setSvg}
+                    //   setPath={setPath}
+                    setHasValidated={setHasValidated}
+                    finishCountdownDate={finishCountdownDate}
+                  />
+                </>
+              ) : (
+                <div className="flex justify-center">
+                  Hop ! C&apos;est envoyé ! On attend les autres...
+                </div>
+              )}
+
+              <div className="flex justify-center">
+                <CountDown finishCountdownDate={finishCountdownDate} />
+              </div>
+            </>
+          )}
+
+          {phase === "guessing" &&
+            lastLink?.type === "draw" &&
+            lastLink?.data.startsWith("data:image") && (
+              <>
+                <div
+                  style={{
+                    position: "relative",
+                    width: "auto",
+                    height: "50vh",
+                    // left: "5vw",
+                  }}
+                >
+                  <Image
+                    src={lastLink.data}
+                    alt="drawing-png"
+                    // sizes="500px"
+                    fill
+                    style={{
+                      objectFit: "contain",
+                    }}
+                  />
+                </div>
+                {!hasValidated ? (
+                  <>
+                    <div>Qu&apos;est-ce que ça représente ???</div>
+                    <input
+                      type="text"
+                      value={guess}
+                      onChange={(e) => setGuess(e.target.value)}
+                      className="border focus:outline-none focus:border-2"
+                    />
+
+                    <button
+                      onClick={() => {
+                        setData(guess);
+                        setHasValidated(true);
+                      }}
+                      className="border border-blue-300 bg-blue-100"
+                    >
+                      Envoi
+                    </button>
+                  </>
+                ) : (
+                  <div>Envoyé ! On attend les autres...</div>
+                )}
+              </>
+            )}
+
+          {phase.startsWith("showing") && (
+            <>
+              <div>Chaîne de {showedGamer}</div>
+
+              {showedLinks && (
+                <div>
+                  {showedLinks.map((link, i) => (
+                    <>
+                      {link.type === "draw" ? (
+                        <div key={i}>
+                          <div>Dessin de {link.userName}</div>
+                          <div
+                            style={{
+                              position: "relative",
+                              width: "auto",
+                              height: "50vh",
+                              // left: "5vw",
+                            }}
+                          >
+                            <Image
+                              src={link.data}
+                              alt="drawing-png"
+                              // sizes="500px"
+                              fill
+                              style={{
+                                objectFit: "contain",
+                              }}
+                            />
+                          </div>
+                        </div>
+                      ) : showedGamer !== link.userName ? (
+                        <div key={i}>
+                          {link.userName} pense qu&apos;il s&apos;agit de :{" "}
+                          {link.data}
+                        </div>
+                      ) : (
+                        <div key={i}>
+                          Et il s&apos;agissait de :{" "}
+                          <span className="font-semibold">{link.data}</span>
+                        </div>
+                      )}
+                    </>
+                  ))}
+                </div>
+              )}
+
+              {isShower && (
+                <div className="fixed bottom-0 z-10 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+                  <NextStep
+                    onClick={() => {
+                      goNextShow({ roomToken, gameData });
+                    }}
+                  >
+                    Suite
+                  </NextStep>
+                </div>
+              )}
+            </>
           )}
         </>
       )}
-    </>
+    </div>
   );
 }
