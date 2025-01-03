b086f539a8e3a8ca960d524aac04bed70f478f49
Code <Code@MacBook-Pro-de-Martin.local>
2025-01-03 16:36:31 +0100

    usePreventScroll: disableScrollStyles

--- src/app/utils/usePreventScroll.js
+++ src/app/utils/usePreventScroll.js
@@ -6,15 +6,31 @@ export default function usePreventScroll() {
   useEffect(() => {
     const preventDefault = (e) => e.preventDefault();
 
-    window.addEventListener("wheel", preventDefault, { passive: false });
-    window.addEventListener("touchmove", preventDefault, { passive: false });
-
     const stayAtTop = () => {
       window.scrollTo(0, 0);
     };
+
+    window.addEventListener("wheel", preventDefault, { passive: false });
+    window.addEventListener("touchmove", preventDefault, { passive: false });
     window.addEventListener("scroll", stayAtTop);
 
+    const disableScrollStyles = () => {
+      document.body.style.overflow = "hidden";
+      document.body.style.position = "fixed";
+      document.body.style.width = "100%";
+      document.body.style.height = "100%";
+    };
+    disableScrollStyles();
+
+    const resetScrollStyles = () => {
+      document.body.style.overflow = "";
+      document.body.style.position = "";
+      document.body.style.width = "";
+      document.body.style.height = "";
+    };
+
     return () => {
+      resetScrollStyles();
       window.removeEventListener("wheel", preventDefault);
       window.removeEventListener("touchmove", preventDefault);
       window.removeEventListener("scroll", stayAtTop);
