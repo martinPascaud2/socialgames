7cfda41e5afb57a435b42fa57541d0525d0c7eab
Code <Code@MacBook-Pro-de-Martin.local>
2025-05-05 20:15:57 +0200

    TriactionKeyboard: fix (pending)

--- src/app/categories/[categorie]/(games)/triaction/Triaction.js
+++ src/app/categories/[categorie]/(games)/triaction/Triaction.js
@@ -340,17 +340,18 @@ export default function Triaction({
     //     JSON.stringify({ actions, roomToken })
     //   );
     // }
-    const timeout = setTimeout(() => {
-      if (Object.keys(actions).length) {
-        localStorage.setItem(
-          "SGTriaction_writtenActions",
-          JSON.stringify({ actions, roomToken })
-        );
-      }
-    }, 500);
+    // const timeout = setTimeout(() => {
+    //   if (Object.keys(actions).length) {
+    //     localStorage.setItem(
+    //       "SGTriaction_writtenActions",
+    //       JSON.stringify({ actions, roomToken })
+    //     );
+    //   }
+    // }, 500);
 
-    return () => clearTimeout(timeout);
-  }, [actions, roomToken]);
+    // return () => clearTimeout(timeout);
+    // }, [actions, roomToken]);
+  }, [actions]);
 
   const sendBack = async () => {
     setSentBack(selected);
