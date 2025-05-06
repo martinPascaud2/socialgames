cae8623ae64f1020379efadfd4b165dd31c3e217
Code <Code@MacBook-Pro-de-Martin.local>
2025-05-06 12:33:54 +0200

    triaction: oui_button fix

--- src/app/categories/[categorie]/(games)/triaction/Triaction.js
+++ src/app/categories/[categorie]/(games)/triaction/Triaction.js
@@ -659,7 +659,7 @@ export default function Triaction({
                   <div className="absolute w-full h-full">
                     <div className="w-full h-full relative">
                       <div
-                        onClick={() => confirm()}
+                        onClick={async () => await confirm()}
                         className="absolute left-[20%] top-1/2 translate-y-[-50%] border border-lime-800 bg-lime-100 text-lime-800 py-1 px-2 font-semibold rounded-md"
                       >
                         Oui
