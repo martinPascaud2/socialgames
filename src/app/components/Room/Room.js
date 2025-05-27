a091a2a4bfcdf133c1c4b986fe3f3fc710f08a8c
Code <Code@MacBook-Pro-de-Martin.local>
2025-05-27 12:36:32 +0200

    ThreeSmoke: fix (pending)

--- src/app/components/Room/Room.js
+++ src/app/components/Room/Room.js
@@ -2083,7 +2083,7 @@ export default function Room({
     return (
       // <div className="absolute h-screen w-full z-50 bg-black">
       // <div className="relative h-screen w-full z-50 bg-black">
-      <div className="relative h-screen w-full">
+      <div className="relative h-full w-full">
         <ThreeSmoke />
         <UserContext.Provider value={{ userParams }}>
           <div
